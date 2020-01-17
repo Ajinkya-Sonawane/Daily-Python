3f3d45b6af47c2e4b16d64f53c72029746bfece9
Ajinkya-Sonawane <sonawaneajinks@gmail.com>
2020-01-18 01:21:54 +0530

    Added Article 14

--- 12_DS_Part_1/12_DS_Part_1.py
+++ 12_DS_Part_1/12_DS_Part_1.py
@@ -1,158 +1,100 @@
-# #Implementing basic data structures in Python - LinkedList
-# class Node:
+#Implementing basic data structures in Python - LinkedList
+class Node:
   
-#   def __init__(self,value):
-#     self.value = value
-#     self.nextNode = None
+  def __init__(self,value):
+    self.value = value
+    self.nextNode = None
 
-# class LinkedList:
+class LinkedList:
 
-#   def __init__(self):
-#     self.headNode = None
-#     self.lastNode = None
+  def __init__(self):
+    self.headNode = None
+    self.lastNode = None
 
-#   def insertNode(self,value):
-#     node = Node(value)
-#     if self.headNode == None:
-#       self.headNode = node
-#       self.lastNode = node
-#     else:
-#       self.lastNode.nextNode = node
-#       self.lastNode = node
+  def insertNode(self,value):
+    node = Node(value)
+    if self.headNode == None:
+      self.headNode = node
+      self.lastNode = node
+    else:
+      self.lastNode.nextNode = node
+      self.lastNode = node
 
-#   def insertNodeAfter(self,value,refValue):
-#     node = Node(value)
-#     if self.headNode == None:
-#       self.headNode = node
-#       self.lastNode = node
-#     else:
-#       iterator = self.headNode
-#       while iterator.value != refValue:
-#         iterator = iterator.nextNode
-#       node.nextNode = iterator.nextNode
-#       iterator.nextNode = node
+  def insertNodeAfter(self,value,refValue):
+    node = Node(value)
+    if self.headNode == None:
+      self.headNode = node
+      self.lastNode = node
+    else:
+      iterator = self.headNode
+      while iterator.value != refValue:
+        iterator = iterator.nextNode
+      node.nextNode = iterator.nextNode
+      iterator.nextNode = node
 
-#   def traverseList(self):
-#     iterator = self.headNode
-#     while iterator != None:
-#       print(iterator.value)
-#       iterator = iterator.nextNode
+  def traverseList(self):
+    iterator = self.headNode
+    while iterator != None:
+      print(iterator.value)
+      iterator = iterator.nextNode
 
-#   def deleteNode(self,value):
-#     iterator = self.headNode
-#     while iterator.nextNode.value != value:
-#       iterator = iterator.nextNode
-#     temp = iterator.nextNode
-#     iterator.nextNode = temp.nextNode
-#     del(temp)
-#     print(value,' deleted successfully')
+  def deleteNode(self,value):
+    iterator = self.headNode
+    while iterator.nextNode.value != value:
+      iterator = iterator.nextNode
+    temp = iterator.nextNode
+    iterator.nextNode = temp.nextNode
+    del(temp)
+    print(value,' deleted successfully')
 
-# ll = LinkedList()
-# ll.insertNode(1)
-# ll.insertNode(2)
-# ll.insertNode(3)
-# ll.traverseList()
-# ll.deleteNode(2)
-# ll.traverseList()
-# ll.insertNodeAfter(2,1)
-# ll.traverseList()
-
-
+ll = LinkedList()
+ll.insertNode(1)
+ll.insertNode(2)
+ll.insertNode(3)
+ll.traverseList()
+ll.deleteNode(2)
+ll.traverseList()
+ll.insertNodeAfter(2,1)
+ll.traverseList()
 
 
 
-# #Implementing basic data structures in Python - LinkedList
-# class Node:
+
+
+
+
+
+#Implementing basic data structures in Python - Stack
+class Stack:
+
+  def __init__(self):
+    self.top = -1
+    self.data = []
+
+  def push(self,value):
+    print(value,' pushed successfully' )    
+    self.data.append(value)
+    self.top +=1 
   
-#   def __init__(self,value):
-#     self.value = value
-#     self.nextNode = None
-# class LinkedList:
-#   def __init__(self):
-#     self.headNode = None
-#     self.lastNode = None
-#   def insertNode(self,value):
-#     node = Node(value)
-#     if self.headNode == None:
-#       self.headNode = node
-#       self.lastNode = node
-#     else:
-#       self.lastNode.nextNode = node
-#       self.lastNode = node
-#     print(value,'inserted successfully')
+  def pop(self):
+    if self.top == -1:
+        print('Stack is empty')        
+        return 
+    print(self.data[self.top],' popped successfully')
+    del self.data[self.top]
+    self.top -= 1
 
-#   def insertNodeAfter(self,value,refValue):
-#     node = Node(value)
-#     if self.headNode == None:
-#       self.headNode = node
-#       self.lastNode = node
-#     else:
-#       iterator = self.headNode
-#       while iterator.value != refValue:
-#         iterator = iterator.nextNode
-#       node.nextNode = iterator.nextNode
-#       iterator.nextNode = node
-#     print(value,'inserted successfully')
+  def printStack(self):
+    print('Stack : ',self.data)
 
-#   def traverseList(self):
-#     iterator = self.headNode
-#     while iterator != None:
-#       print(iterator.value)
-#       iterator = iterator.nextNode
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(2)
+stack.printStack()
+stack.pop()
+stack.printStack()
 
-#   def deleteNode(self,value):
-#     iterator = self.headNode
-#     while iterator.nextNode.value != value:
-#       iterator = iterator.nextNode
-#     temp = iterator.nextNode
-#     iterator.nextNode = temp.nextNode
-#     del(temp)
-#     print(value,' deleted successfully')
-
-# ll = LinkedList()
-# ll.insertNode(1)
-# ll.insertNode(2)
-# ll.insertNode(3)
-# ll.traverseList()
-# ll.deleteNode(2)
-# ll.traverseList()
-# ll.insertNodeAfter(2,1)
-# ll.traverseList()
-
-
-
-
-
-# #Implementing basic data structures in Python - Stack
-# class Stack:
-
-#   def __init__(self):
-#     self.top = -1
-#     self.data = []
-
-#   def push(self,value):
-#     print(value,' pushed successfully' )    
-#     self.data.append(value)
-#     self.top +=1 
-  
-#   def pop(self):
-#     if self.top == -1:
-#         print('Stack is empty')        
-#         return 
-#     print(self.data[self.top],' popped successfully')
-#     del self.data[self.top]
-#     self.top -= 1
-
-#   def printStack(self):
-#     print('Stack : ',self.data)
-
-# stack = Stack()
-# stack.push(1)
-# stack.push(2)
-# stack.push(2)
-# stack.printStack()
-# stack.pop()
-# stack.printStack()
 
 
 class Queue:
